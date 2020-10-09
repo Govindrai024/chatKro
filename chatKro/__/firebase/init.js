92c653adf347e430aea7bd6414e0b4524c6be694
Govind Rai Rathore <58888816+Govindrai024@users.noreply.github.com>
2020-10-09 14:27:45 +0500

    Add files via upload

---
+++ chatKro/__/firebase/init.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBMjeLTBhsNInnOtNa02RPTyVSVtMENrbM",
+    authDomain: "chatkro-c3141.firebaseapp.com",
+    databaseURL: "https://chatkro-c3141.firebaseio.com",
+    projectId: "chatkro-c3141",
+    storageBucket: "chatkro-c3141.appspot.com",
+    messagingSenderId: "934198550280",
+    appId: "1:934198550280:web:993b27c2db22dcb63856bd",
+    measurementId: "G-FV4FBHFVQ1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
